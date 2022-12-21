6c1b7e50d231b378d50fd546cc9d8f2434cd0b56
Sergio Cainzo <97249973+SergioCainzo@users.noreply.github.com>
2022-12-21 16:01:56 -0300

    Update jogo_do_bicho.py

--- jogo_do_bicho.py
+++ jogo_do_bicho.py
@@ -3,6 +3,8 @@ print('Jogo do Bicho!!');
 
 #importações
 import random
+import time
+
 
 #regras
 print('''
@@ -13,70 +15,93 @@ O Jogo do bicho é um jogo de aposta e escolha.
 3 - O resultado sai logo em seguida.
 ''');
 
+#Variável externa
+max_tentativa = False;
+t = 4;
 
 #Opção de jogo
-print('''
+while True:
+        
+    print('''
 Qual dos bichos você escolhe?
 [1] - Leão
 [2] - Tigre
 [3] - Coelho
 [4] - Burro
-''')
-opcao = int(input('Qual animal você escolhe: '));
+    ''')
+    opcao = int(input('Qual animal você escolhe: '));
 
-if opcao == 1:
-    opcao = 0;
-elif opcao == 2:
-    opcao = 1;
-elif opcao == 3:
-    opcao = 2;
-elif opcao == 4:
-    opcao = 3;
-else:
-    print('Opção inválida.');
+    if opcao > 4 or opcao == 0:
+        t = t-1
+        time.sleep(1)
+        print('\n');
+        print(f'Opção Inválida, tente novamente.\nVocê possui {t} tentativas');
+        print('\n');
+        if t == 0:
+            print('Você excedeu a quantidadade de tentativas.');
+            print('\n');
+            max_tentativa = True;
+            break;
 
-#Variáveis e Lista
-sorteio = random.randint(0,3);
-lista_bicho = ['Leão', 'Tigre', 'Coelho', 'Burro'];
+    #Variáveis e Lista
+    sorteio = random.randint(0,3);
+    lista_bicho = ['Leão', 'Tigre', 'Coelho', 'Burro'];
+
+    time.sleep(1)
+
+    # Leão
+    if opcao == 1:
+        print(f'Você escolheu: {lista_bicho[0]}');
+        if sorteio == 0:
+            time.sleep(1)
+            print(f'O bicho sorteado foi: {lista_bicho[sorteio]}');
+            print('Você acertou!!!');
+            time.sleep(1)
+        else:
+            time.sleep(1)
+            print(f'O bicho sorteado foi: {lista_bicho[sorteio]}');
+            print('Você errou. Mais sorte na proxima');
+            time.sleep(1)
 
 
-# Leão
-if opcao == 0:
-    print(f'Você escolheu: {lista_bicho[opcao]}');
-    if sorteio == 0:
-        print(f'O bicho sorteado foi: {lista_bicho[sorteio]}');
-        print('Você acertou!!!');
-    else:
-        print(f'O bicho sorteado foi: {lista_bicho[sorteio]}');
-        print('Você errou. Mais sorte na proxima');
+    # Tigre
+    if opcao == 2:
+        print(f'Você escolheu: {lista_bicho[1]}');
+        if sorteio == 1:
+            time.sleep(1)
+            print(f'O bicho sorteado foi: {lista_bicho[sorteio]}');
+            print('Você acertou!!!');
+            time.sleep(1)
+        else:
+            time.sleep(1)
+            print(f'O bicho sorteado foi: {lista_bicho[sorteio]}');
+            print('Você errou. Mais sorte na proxima');
+            time.sleep(1)
 
-
-# Tigre
-if opcao == 1:
-    print(f'Você escolheu: {lista_bicho[opcao]}');
-    if sorteio == 1:
-        print(f'O bicho sorteado foi: {lista_bicho[sorteio]}');
-        print('Você acertou!!!');
-    else:
-        print(f'O bicho sorteado foi: {lista_bicho[sorteio]}');
-        print('Você errou. Mais sorte na proxima');
-
-#Coelho
-if opcao == 2:
-    print(f'Você escolheu: {lista_bicho[opcao]}');
-    if sorteio == 2:
-        print(f'O bicho sorteado foi: {lista_bicho[sorteio]}');
-        print('Você acertou!!!');
-    else:
-        print(f'O bicho sorteado foi: {lista_bicho[sorteio]}');
-        print('Você errou. Mais sorte na proxima');
-        
-#Burro
-if opcao == 3:
-    print(f'Você escolheu: {lista_bicho[opcao]}');
-    if sorteio == 3:
-        print(f'O bicho sorteado foi: {lista_bicho[sorteio]}');
-        print('Você acertou!!!');
-    else:
-        print(f'O bicho sorteado foi: {lista_bicho[sorteio]}');
-        print('Você errou. Mais sorte na proxima');
+    #Coelho
+    if opcao == 3:
+        print(f'Você escolheu: {lista_bicho[2]}');
+        if sorteio == 2:
+            time.sleep(1)
+            print(f'O bicho sorteado foi: {lista_bicho[sorteio]}');
+            print('Você acertou!!!');
+            time.sleep(1)
+        else:
+            time.sleep(1)
+            print(f'O bicho sorteado foi: {lista_bicho[sorteio]}');
+            print('Você errou. Mais sorte na proxima');
+            time.sleep(1)
+            
+    #Burro
+    if opcao == 4:
+        print(f'Você escolheu: {lista_bicho[3]}');
+        if sorteio == 3:
+            time.sleep(1)
+            print(f'O bicho sorteado foi: {lista_bicho[sorteio]}');
+            print('Você acertou!!!');
+            time.sleep(1)
+        else:
+            time.sleep(1)
+            print(f'O bicho sorteado foi: {lista_bicho[sorteio]}');
+            print('Você errou. Mais sorte na proxima');
+            time.sleep(1)
